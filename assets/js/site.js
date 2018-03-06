8c7f4870722bcaebfc424d97efcfe4c1198a6177
The Mai <Mai.The.09@gmail.com>
2018-03-06 15:53:54 -0600

    add display() function and added Masonry

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,2 +1,40 @@
-var GphApiClient = require('giphy-js-sdk-core');
-client = GphApiClient("rxJdnZS8wGz7tycEkjccy4PuOCwdp4Us");
+function display(search, addtionalParameters = '') {
+    // Clears current images
+    $('#giphy-view').empty();
+
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=';
+    var apiKey = 'rxJdnZS8wGz7tycEkjccy4PuOCwdp4Us';
+    var search = search;
+    var addtionalParameters = addtionalParameters;
+    $.get(queryURL + search + '&api_key=' + apiKey + addtionalParameters).then(function (response) {
+        console.log(response.data[0]);
+        response.data.forEach(function (i) {
+            $('#giphy-view').append($('<img>').attr({
+                class: "grid-item",
+                src: i.images['480w_still'].url,
+            }).data({
+                still: i.images['480w_still'].url,
+                moving: i.images.original.url,
+                triggered: false
+            }));
+        })
+        $('#giphy-view').imagesLoaded().done(function (instance) {
+            $('.grid').masonry({
+                itemSelector: '.grid-item',
+                columnWidth: 385
+            });
+        })
+    });
+}
+
+$(document).on('click', 'img', function (e) {
+    if ($(this).data('triggered')) {
+        $(this).data('triggered', false);
+        $(this).attr('src', $(this).data('still'));
+    } else {
+        $(this).data('triggered', true);
+        $(this).attr('src', $(this).data('moving'));
+    }
+})
+
+display('duck');
